c71088d3115550723a70edca717a39a0cb80e21f
Aristides Rodríguez <12479256+arisrodriguez@users.noreply.github.com>
2025-09-04 22:20:41 -0500

    Agrega botón de descarga para la guía de testers en Hermes App

--- src/components/DescriptionApp.jsx
+++ src/components/DescriptionApp.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 /**
  * Componente DescriptionApp que proporciona una breve descripción de Hermes App.
  * Contiene un título y un párrafo explicativo sobre la funcionalidad de la aplicación.
@@ -14,7 +12,15 @@ export const DescriptionApp = () => {
             </div>
 
             {/* Descripción de la aplicación */}
-            <p className='text-gray-700 text-base'>Hermes is the ultimate app for optimizing your physical training. Designed for athletes of all levels, Hermes Running Calculator accurately calculates your personalized training zones based on your heart rate, power, or pace. Enter your data and goals, and the app will provide you with specific zones to maximize your performance, enhance your endurance, and achieve your goals more efficiently.</p>
+            <p className='text-gray-700 text-base'>Hermes is the ultimate app for optimizing your physical training. Designed for athletes of all levels, Hermes Running Calculator accurately calculates your personalized training zones based on your heart rate and pace. Enter your data, and the app will provide you with specific zones to maximize your performance, enhance your endurance, and achieve your goals more efficiently.</p>
+
+            <a
+                className="block mx-auto w-3/4 mt-6 text-center border border-blue-700 text-blue-700 rounded-md px-4 py-2 hover:bg-blue-700 hover:text-white transition-colors duration-300"
+                href="/Hermes_Guia_Testers.pdf"
+                download={true}
+            >
+                Descargar manual de tester
+            </a>
         </section>
     )
 }
