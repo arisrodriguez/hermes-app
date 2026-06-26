5569200dbadbcef4229e27809f4f8acd1ac3baf9
Aristides Rodriguez <izzat1029@hotmail.com>
2026-06-25 22:58:00 -0500

    feat(description): mejorar texto CTA, cambiar texto del botón a inglés y añadir ícono de descarga

--- src/components/DescriptionApp.jsx
+++ src/components/DescriptionApp.jsx
@@ -2,6 +2,7 @@
  * Componente DescriptionApp que proporciona una breve descripción de Hermes App.
  * Contiene un título y un párrafo explicativo sobre la funcionalidad de la aplicación.
  */
+import { MdOutlineFileDownload } from "react-icons/md";
 
 export const DescriptionApp = () => {
     return (
@@ -13,13 +14,16 @@ export const DescriptionApp = () => {
 
             {/* Descripción de la aplicación */}
             <p className='text-gray-700 text-base'>Hermes is the ultimate app for optimizing your physical training. Designed for athletes of all levels, Hermes Running Calculator accurately calculates your personalized training zones based on your heart rate and pace. Enter your data, and the app will provide you with specific zones to maximize your performance, enhance your endurance, and achieve your goals more efficiently.</p>
+            <br />
+           <p  className='text-gray-700 text-base'>Download the testing guide, complete the tests, and help improve Hermes.</p>
 
             <a
-                className="block mx-auto w-3/4 mt-6 text-center border border-blue-700 text-blue-700 rounded-md px-4 py-2 hover:bg-blue-700 hover:text-white transition-colors duration-300"
                 href="/Hermes_Guia_Testers.pdf"
-                download={true}
-            >
-                Descargar manual de tester
+                download
+                className="mx-auto mt-6 flex w-fit items-center justify-center gap-2 rounded-md border border-blue-700 px-5 py-3 text-blue-700 transition-colors duration-500 hover:bg-blue-700 hover:text-white hover:tras"
+                >
+                <MdOutlineFileDownload className="text-2xl" />
+                <span className="font-medium">Download Testing Guide</span>
             </a>
         </section>
     )
