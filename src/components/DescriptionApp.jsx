0b68ef1dd5d1f294a3c3e050507acc5a289d2fd9
Aristides Rodriguez <12479256+arisrodriguez@users.noreply.github.com>
2026-06-27 21:05:43 -0500

    style: limpiar espacios en blanco y saltos de línea

--- src/components/DescriptionApp.jsx
+++ src/components/DescriptionApp.jsx
@@ -15,7 +15,7 @@ export const DescriptionApp = () => {
             {/* Descripción de la aplicación */}
             <p className='text-gray-700 text-base'>Hermes is the ultimate app for optimizing your physical training. Designed for athletes of all levels, Hermes Running Calculator accurately calculates your personalized training zones based on your heart rate and pace. Enter your data, and the app will provide you with specific zones to maximize your performance, enhance your endurance, and achieve your goals more efficiently.</p>
             <br />
-           <p  className='text-gray-700 text-base'>Download the testing guide, complete the tests, and help improve Hermes.</p>
+            <p  className='text-gray-700 text-base'>Download the testing guide, complete the tests, and help improve Hermes.</p>
 
             <a
                 href="/Hermes_Guia_Testers.pdf"
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -17,54 +17,54 @@ import { forwardRef } from 'react'
 
 export const Results = forwardRef(({ zones, name }, ref) => {
 
-    // Validación: Verifica si "zones" es un array válido antes de renderizar la tabla.
-    if (!zones || !Array.isArray(zones)) {
-        console.error('Error: "zones" es undefined o no es un array', zones);
-        return null;
-    }
+  // Validación: Verifica si "zones" es un array válido antes de renderizar la tabla.
+  if (!zones || !Array.isArray(zones)) {
+    console.error('Error: "zones" es undefined o no es un array', zones);
+    return null;
+  }
 
-    return (
-        <section ref={ref} id='results' className='bg-blue-700 text-center px-6'>
-            <div className='md:w-[700px] mx-auto'>
-                <div className="text-white pt-16">
-                    <h1 className='font-bold text-4xl mb-5'>Training zones</h1>
-                    <p className='mb-10 text-base'>Hey <strong>{name || "Athlete"}</strong>. Enjoy your training zones
-                        and build your best version</p>
-                </div>
+  return (
+    <section ref={ref} id='results' className='bg-blue-700 text-center px-6'>
+      <div className='md:w-[700px] mx-auto'>
+        <div className="text-white pt-16">
+          <h1 className='font-bold text-4xl mb-5'>Training zones</h1>
+          <p className='mb-10 text-base'>Hey <strong>{name || "Athlete"}</strong>. Enjoy your training zones
+            and build your best version</p>
+        </div>
 
-                {/* Tabla con las zonas de entrenamiento */}
-                <div className='pb-16 text-white'>
-                    <table className="w-full table-fixed border-separate border-spacing-2">
-                        {/*Encabezado de la tabla */}
-                        <thead>
-                            <tr>
-                                <th className="bg-white text-blue-700 font-bold py-2 px-3 rounded w-1/3">Zones</th>
-                                <th className="bg-white text-blue-700 font-bold py-2 px-3 rounded w-1/3">HR (bpm)</th>
-                                <th className="bg-white text-blue-700 font-bold py-2 px-3 rounded w-1/3">Pace (min/km)</th>
-                            </tr>
-                        </thead>
+        {/* Tabla con las zonas de entrenamiento */}
+        <div className='pb-16 text-white'>
+          <table className="w-full table-fixed border-separate border-spacing-2">
+            {/*Encabezado de la tabla */}
+            <thead>
+              <tr>
+                <th className="bg-white text-blue-700 font-bold py-2 px-3 rounded w-1/3">Zones</th>
+                <th className="bg-white text-blue-700 font-bold py-2 px-3 rounded w-1/3">HR (bpm)</th>
+                <th className="bg-white text-blue-700 font-bold py-2 px-3 rounded w-1/3">Pace (min/km)</th>
+              </tr>
+            </thead>
 
-                        {/* Cuerpo de la tabla con las zonas de entrenamiento */}
-                        <tbody>
-                            {zones.map(({ zone, hr, pace }) => (
-                                <tr key={zone}>
-                                    <td className="bg-white text-blue-700 py-2 px-4 rounded">{zone}</td>
-                                    <td className="bg-white text-blue-700 py-2 px-4 rounded">{hr}</td>
-                                    <td className="bg-white text-blue-700 py-2 px-4 rounded">{pace}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-            {/* Botón solo visible cuando hay datos */}
-            {/*<div className="pb-10">
+            {/* Cuerpo de la tabla con las zonas de entrenamiento */}
+            <tbody>
+              {zones.map(({ zone, hr, pace }) => (
+                <tr key={zone}>
+                  <td className="bg-white text-blue-700 py-2 px-4 rounded">{zone}</td>
+                  <td className="bg-white text-blue-700 py-2 px-4 rounded">{hr}</td>
+                  <td className="bg-white text-blue-700 py-2 px-4 rounded">{pace}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      {/* Botón solo visible cuando hay datos */}
+      {/*<div className="pb-10">
                 <button
                     className="bg-white text-blue-700 font-bold py-2 px-4 rounded hover:bg-gray-300 transition cursor-pointer"
                 >
                     Download as Image
                 </button>
             </div>*/}
-        </section>
-    )
+    </section>
+  )
 })
